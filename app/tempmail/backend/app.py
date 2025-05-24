472b758aae31b1508a11d4131aed2dcce70eda56
Olaf Sarnow <olsarnow@gmail.com>
2025-05-24 09:12:18 +0200

    modsecurity + 403.html

--- app/tempmail/backend/app.py
+++ app/tempmail/backend/app.py
@@ -211,6 +211,11 @@ def stats():
 def not_found_error(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(403)
+def forbidden_error(error):
+    return render_template('403.html'), 403
+
+
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
